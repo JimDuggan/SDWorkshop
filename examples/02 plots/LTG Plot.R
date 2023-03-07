ce1000786cd7f60b829627047dc245374416a699
Jim Duggan <jim.duggan@nuigalway.ie>
2023-03-07 20:54:49 +0000

    Adding early examples

---
+++ examples/02 plots/LTG Plot.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(bbplot)
+
+
+d <- read_csv("datasets/single run/LTG.csv")
+
+ggplot(d,aes(x=Year,y=Population)) + 
+  geom_point()+geom_line()+bbc_style()
+
+
+ggplot(d,aes(x=Year,y=Population)) + 
+  geom_point()+geom_line()
+
+d <- d %>%
+  mutate(ModeValue=round(c(NA,NA,diff(abs(diff(x)))),3),
+         Mode=as.factor(ifelse(ModeValue>0,"EXP",
+                     ifelse(ModeValue<0,"LOG","LIN"))))
+
+ggplot(d,aes(x=Year,y=Population,colour=Mode)) + 
+  geom_point()+theme_light()
+
+ggplot(d,aes(x=Year,y=Population,colour=Mode)) + 
+  geom_point()+geom_line()+theme_classic()
+
+ggplot(d,aes(x=Year,y=Population,colour=Mode)) + 
+  geom_point()+geom_line()+theme_classic()+facet_wrap(~Mode)
+
+ggplot(d,aes(x=Additions,y=Population,colour=Mode))+geom_point()+bbc_style()
+
+
---
+++ examples/03 Sensitivity/LTG Summaries.R
@@ -0,0 +1,39 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+# Load in the sensitivity data
+sd <- read_csv("datasets/sensitivity/LTG_Sensitivity.csv")
+
+# Convert to tidy data format
+sd_tidy <- sd %>%
+             pivot_longer(cols=!Year,names_to="Variable",values_to="Value") %>%
+             mutate(Variable=str_replace_all(Variable,'\"',""),
+                    Variable=str_replace(Variable,"=","")) %>%
+             separate(Variable,into=c("Run","Variable"),sep = ":") %>%
+             mutate(Variable=trimws(Variable)) %>%
+             separate(Run,c("TempRun","Run"),sep = " ",convert = TRUE) %>%
+             select(Run,Year,Variable,Value)
+
+ggplot(filter(sd_tidy,Variable=="Population"),
+       aes(x=Year,y=Value,colour=Run,group=Run))+geom_line()
+
+ 
+sum_runs <- sd_tidy %>%
+              filter(Variable=="Population") %>%
+              group_by(Variable,Year) %>%
+              summarise(Q75=quantile(Value,0.75),
+                        Q25=quantile(Value,0.25),
+                        Median=median(Value),
+                        Mean=mean(Value))
+
+
+ggplot(sum_runs,aes(Year, Median)) + 
+  geom_ribbon(aes(ymin = Q25,
+                  ymax = Q75),
+              fill = "steelblue2") + 
+  geom_line(color = "firebrick")
+
+
+            
